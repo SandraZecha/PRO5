3480d7ab38b563f063d7e1ce01975dcee798410b
SandraZecha <contact@sandrazecha.com>
2018-10-31 14:16:15 +0100

    dynamic cocktail switching & base structure ingredients

--- js/main.js
+++ js/main.js
@@ -1,37 +1,47 @@
-//var allDivs = $( "div" );
-var cocktailList = $(".cocktail-indices").find("div"), current = 0, newCocktail = 0, newClass = "";
+var cocktailList = $('.cocktail-indices').children(), current;
 
+//updates the "current" variable to get the index of the current Cocktail
+function getCurrentIndex () {
+    let currentClass = $("#main-wrapper").attr("class");
+
+    cocktailList.each(function( ) {
+        if( $(this).attr("class") === currentClass ) {
+            current = $(this).index();
+        }
+    });
+}
 
 $(document).ready(function () {
+    let newCocktail, newClass;
 
+    //Change to prev Cocktail on click
     $(".prev a").on("click", function () {
-        var currentClass = $("#main-wrapper").attr("class");
-        current = cocktailList.indexOf(currentClass);
+        getCurrentIndex();
+
         if(current <= 0){
             newCocktail = 1;
         } else {
             newCocktail = current - 1;
         }
-        alert(newCocktail);
-        newClass = cocktailList[newCocktail].attr("class");
+        newClass = cocktailList.eq(newCocktail).attr("class");
         $("#main-wrapper").attr("class", newClass);
+        $(".main-heading h1").text(newClass);
 
-        //$("#main-wrapper").removeClass("mojito").addClass("bloodymary");
-        //$(".main-heading h1").text("Bloody Mary");
     });
 
+    //Change to next Cocktail on click
     $(".next a").on("click", function () {
-        current = cocktailList.indexOf($("#main-wrapper").attr("class"));
+        getCurrentIndex();
+
         if(current >= 1){
             newCocktail = 0;
         } else {
             newCocktail = current + 1;
         }
-        alert(newCocktail);
-        $("#main-wrapper").attr("class", cocktailList[newCocktail].attr("class"));
+        newClass = cocktailList.eq(newCocktail).attr("class");
+        $("#main-wrapper").attr("class", newClass);
+        $(".main-heading h1").text(newClass);
 
-        //$("#main-wrapper").removeClass("bloodymary").addClass("mojito");
-        //$(".main-heading h1").text("Mojito");
     });
 
 });
