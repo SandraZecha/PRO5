63c75c0847f4854e6aaf83f3eec5d8c1d61a478b
SandraZecha <contact@sandrazecha.com>
2018-10-31 12:34:05 +0100

    js stuff

--- js/main.js
+++ js/main.js
@@ -1,13 +1,37 @@
+//var allDivs = $( "div" );
+var cocktailList = $(".cocktail-indices").find("div"), current = 0, newCocktail = 0, newClass = "";
+
+
 $(document).ready(function () {
 
     $(".prev a").on("click", function () {
-        $("#main-wrapper").removeClass("mojito").addClass("bloodymary");
-        $(".main-heading h1").text("Bloody Mary");
+        var currentClass = $("#main-wrapper").attr("class");
+        current = cocktailList.indexOf(currentClass);
+        if(current <= 0){
+            newCocktail = 1;
+        } else {
+            newCocktail = current - 1;
+        }
+        alert(newCocktail);
+        newClass = cocktailList[newCocktail].attr("class");
+        $("#main-wrapper").attr("class", newClass);
+
+        //$("#main-wrapper").removeClass("mojito").addClass("bloodymary");
+        //$(".main-heading h1").text("Bloody Mary");
     });
 
     $(".next a").on("click", function () {
-        $("#main-wrapper").removeClass("bloodymary").addClass("mojito");
-        $(".main-heading h1").text("Mojito");
+        current = cocktailList.indexOf($("#main-wrapper").attr("class"));
+        if(current >= 1){
+            newCocktail = 0;
+        } else {
+            newCocktail = current + 1;
+        }
+        alert(newCocktail);
+        $("#main-wrapper").attr("class", cocktailList[newCocktail].attr("class"));
+
+        //$("#main-wrapper").removeClass("bloodymary").addClass("mojito");
+        //$(".main-heading h1").text("Mojito");
     });
 
 });
